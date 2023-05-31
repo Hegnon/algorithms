93924e3754723bf88416f8f3c3f384834b7431e8
Hegnon <hegnondutra@gmail.com>
2023-05-31 07:32:20 -0300

    inciando a criação de um select sort. algoritimo nivel 2

--- select_sort.rb
+++ select_sort.rb
@@ -1,1 +1,21 @@
 def select (arr)
+  n = arr.length
+
+  for i in 0..(, -2)
+    min_index = i
+
+    for j in (i + 1)..(n - i)
+      if arr[j] < arr[min_index]
+        min_index = j
+      end
+    end
+  
+    if min_index != i
+      arr[i], arr[min_index] = arr[min_index], arr[i]
+    end
+  end
+  
+  return Arr
+end
+
+
