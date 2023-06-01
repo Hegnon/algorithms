8c2e80621da1f2a0dac066ee2e11723d717ff2cd
Hegnon <hegnondutra@gmail.com>
2023-06-01 19:13:39 -0300

    Concluindo  select sort. algoritimo nivel 2

--- select_sort.rb
+++ select_sort.rb
@@ -1,21 +1,24 @@
 def select (arr)
   n = arr.length
 
-  for i in 0..(, -2)
+  for i in 0..(n - 2)
     min_index = i
 
-    for j in (i + 1)..(n - i)
-      if arr[j] < arr[min_index]
+    for j in (i + 1)..(n - 1)
+      if arr[j].nil? || (arr[min_index] && arr[j] < arr[min_index])
         min_index = j
       end
     end
-  
+
     if min_index != i
       arr[i], arr[min_index] = arr[min_index], arr[i]
     end
   end
-  
-  return Arr
+
+  return arr
 end
 
-
+# Exemplo de uso:
+array = [64, 25, 12, 22, 11]
+sorted_array = select (array)
+puts sorted_array.inspect
