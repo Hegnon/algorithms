e1909186680482545e30389c560bf66679fb6c7b
Hegnon <hegnondutra@gmail.com>
2023-06-08 22:30:32 -0300

    criando algoritimo jump search

---
+++ jump_search.rb
@@ -0,0 +1,33 @@
+def jump(array, alvo)
+    n = array.length
+    step = Math.sqrt(n).to_i
+    prev = 0
+
+    while array[[step, n].min - 1] < alvo 
+        prev = step
+        step += Math.sqrt(n).to_i
+        return -1 if prev >= n
+
+        break if array[[step, n].min - 1] >= alvo
+    end
+
+    while array[prev] < alvo
+        prev += 1
+        return -1 if prev == [step, n].min    
+    end
+
+    return prev if array[prev] == alvo
+    return -1
+end
+
+
+array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+alvo = 15
+
+result = jump(array, alvo)
+
+if result == -1
+  puts "Elemento não encontrado no array."
+else
+  puts "Elemento encontrado no índice #{result}."
+end
